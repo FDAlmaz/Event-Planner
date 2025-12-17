943c32b689035ae93ad27af1074c6ff18f4dea38
Anton Bolkunevich <balkon2006@gmail.com>
2025-12-17 23:34:45 +0300

    Configure admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Event, Participant
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location', 'created_by', 'registered_count')
+    list_filter = ('date',)
+    search_fields = ('title', 'description', 'location')
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'registered_at')
+    list_filter = ('event', 'registered_at')
